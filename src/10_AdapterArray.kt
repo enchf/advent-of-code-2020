17d0b6a0286e4cc410cb715569c07fff3e021b92
Ernesto Espinosa <ernesto@helix.re>
2020-12-24 12:25:14 -0600

    Day 10 - Part 2 problem structure

--- src/10_AdapterArray.kt
+++ src/10_AdapterArray.kt
@@ -1,4 +1,5 @@
 import utils.fileLines
+import java.math.BigDecimal
 
 /**
  * --- Day 10: Adapter Array ---
@@ -109,8 +110,62 @@ import utils.fileLines
  * the adapters, and your device.
  *
  * What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?
+ *
+ * --- Part Two ---
+ *
+ * To completely determine whether you have enough adapters,
+ * you'll need to figure out how many different ways they can be arranged.
+ * Every arrangement needs to connect the charging outlet to your device.
+ * The previous rules about when adapters can successfully connect still apply.
+ *
+ * The first example above (the one that starts with 16, 10, 15) supports the following arrangements:
+ *
+ * (0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+ * (0), 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, (22)
+ * (0), 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, (22)
+ * (0), 1, 4, 5, 7, 10, 12, 15, 16, 19, (22)
+ * (0), 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+ * (0), 1, 4, 6, 7, 10, 12, 15, 16, 19, (22)
+ * (0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)
+ * (0), 1, 4, 7, 10, 12, 15, 16, 19, (22)
+ *
+ * (The charging outlet and your device's built-in adapter are shown in parentheses.)
+ * Given the adapters from the first example, the total number of arrangements that connect the charging outlet to your device is 8.
+ *
+ * The second example above (the one that starts with 28, 33, 18) has many arrangements. Here are a few:
+ *
+ * (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+ * 32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 48, 49, (52)
+ * (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+ * 32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 49, (52)
+ * (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+ * 32, 33, 34, 35, 38, 39, 42, 45, 46, 48, 49, (52)
+ * (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+ * 32, 33, 34, 35, 38, 39, 42, 45, 46, 49, (52)
+ * (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+ * 32, 33, 34, 35, 38, 39, 42, 45, 47, 48, 49, (52)
+ * (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+ * 46, 48, 49, (52)
+ * (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+ * 46, 49, (52)
+ * (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+ * 47, 48, 49, (52)
+ * (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+ * 47, 49, (52)
+ * (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+ * 48, 49, (52)
+ *
+ * In total, this set of adapters can connect the charging outlet to your device in 19208 distinct arrangements.
+ *
+ * You glance back down at your bag and try to remember why you brought so many adapters;
+ * there must be more than a trillion valid ways to arrange them!
+ * Surely, there must be an efficient way to count the arrangements.
+ *
+ * What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device?
  */
+fun List<Int>.backTrackPossibilities(): BigDecimal = BigDecimal(size)
+
 fun main() = fileLines("src/10_AdapterArray.txt", "src/10_Sample.txt", "src/10_Sample2.txt") { it.toInt() }
-    .map { it.plus(0).sorted().let { list -> list.plus(list.last() + 3) } }
-    .map { it.zipWithNext().groupBy { (a, b) -> b - a }.map { (_, list) -> list.size }.reduce(Int::times) } // Part 1
-    .forEach(::println)
+    .map { it.plus(0).sorted().let { list -> list.plus(list.last() + 3) } } // Sorted list plus 0 and device.
+    .onEach { it.zipWithNext().groupBy { (a, b) -> b - a }.map { (_, list) -> list.size }.reduce(Int::times).let(::println) } // Part 1
+    .forEach { it.backTrackPossibilities().let(::println) } // Part 2
