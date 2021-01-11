49e3cf710a54fbb7efac40dd1c23f206b12e2c0b
Ernesto Espinosa <ernesto@helix.re>
2021-01-11 16:59:50 -0600

    Infinite loop to play Game of Life evolution

--- src/complexity/GameOfLife.kt
+++ src/complexity/GameOfLife.kt
@@ -12,6 +12,13 @@ fun <T> GameOfLife<T>.stabilize() = generateSequence(this) { it.evolve() }
     .find { (before, after) -> before.matrix == after.matrix }
     ?.second ?: error("Infinite loop")
 
+// Rewind cursor not working in IntelliJ console.
+fun <T> GameOfLife<T>.rewindCursor() = repeat(matrix.size) { print("\u001B[1A") }
+fun <T> GameOfLife<T>.print() = matrix.forEach { row -> row.forEach { print(it) }.also { println() } }
+
+fun <T> GameOfLife<T>.play() = generateSequence(this) { it.evolve() }
+    .forEach { it.run { print().also { rewindCursor() }.also { Thread.sleep(666) } } }
+
 fun <T> GameOfLife<T>.validX(x: Int) = x >= 0 && x < matrix.size
 fun <T> GameOfLife<T>.validY(y: Int) = y >= 0 && y < matrix.first().size
 fun <T> GameOfLife<T>.validCoordinates(x: Int, y: Int) = validX(x) && validY(y)
