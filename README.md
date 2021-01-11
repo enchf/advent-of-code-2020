2e67ddc85e9aeda41176469c586b4a16e2a96f1d
Ernesto Espinosa <ernesto@helix.re>
2021-01-11 17:21:23 -0600

    Add script to run any class through command line

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # advent-of-code-2020
 
 Pure Kotlin Advent of Code!
+
+## Run in command line.
+
+```
+bash run.sh <name-of-the-kotlin-class>
+```
+
+A file called execution.jar will be generated.
+Kotlin needs to be installed. Installing through `sdkman` is suggested.
--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 KT_CLASSPATH=
-
 while IFS= read -r -d '' ktfile; do KT_CLASSPATH="${KT_CLASSPATH} ${ktfile}"; done < <(find . -name "*.kt" -print0)
 
-echo "${KT_CLASSPATH}"
+CLASS="_$1Kt"
+
+# shellcheck disable=SC2086
+kotlinc $KT_CLASSPATH -d execution.jar
+# shellcheck disable=SC2086
+kotlin -classpath execution.jar $CLASS
